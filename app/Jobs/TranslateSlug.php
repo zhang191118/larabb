64aa0e413ecab59acc475ce8e2b18800a45d4c63
zgl <1017093063@qq.com>
2018-11-08 21:23:13 +0800

    list user replies

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use Illuminate\Support\Facades\DB;
 
 class TranslateSlug implements ShouldQueue
 {
@@ -28,7 +29,8 @@ class TranslateSlug implements ShouldQueue
         // 请求百度 API 接口进行翻译
         $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
 
+
         // 为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
-        \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
+        DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
     }
 }
